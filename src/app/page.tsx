5122a86e604bca170b295254000823473961dc44
Marcos Tavares <mjtdes.md@gmail.com>
2024-05-27 14:59:35 -0300

    feat: Create most of the footer, will continue and make it better.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,75 @@
+import Logo from './Logo'
+
 export default function Home() {
   return (
-    <div>
-      <h1>Testando</h1>
-    </div>
+    <>
+      <div>
+        <Logo />
+        <nav></nav>
+        <div></div>
+        <div></div>
+      </div>
+      <div className="bg-euphoria-gray-800 text-euphoria-gray-100 px-28 py-16">
+        <div className="px-20 pb-12">
+          <section className="flex space-x-20">
+            <div className="flex flex-col space-y-4">
+              <h2 className="text-3xl/10 font-bold">Need Help</h2>
+              <ul className="space-y-1">
+                <li className="text-lg/8">Contact Us</li>
+                <li className="text-lg/8">Track Order</li>
+                <li className="text-lg/8">Returns & Refunds</li>
+                <li className="text-lg/8">FAQ&apos;s</li>
+                <li className="text-lg/8">Career</li>
+              </ul>
+            </div>
+            <div className="flex flex-col space-y-4">
+              <h2 className="text-3xl/8 font-bold">Company</h2>
+              <ul className="space-y-1">
+                <li className="text-lg/8">About Us</li>
+                <li className="text-lg/8">euphoria Blog</li>
+                <li className="text-lg/8">euphoriastan</li>
+                <li className="text-lg/8">Collaboration</li>
+                <li className="text-lg/8">Media</li>
+              </ul>
+            </div>
+            <div className="flex flex-col space-y-4">
+              <h2 className="text-3xl/8 font-bold">More Info</h2>
+              <ul className="space-y-1">
+                <li className="text-lg/8">Term and Conditions</li>
+                <li className="text-lg/8">Privacy Policy</li>
+                <li className="text-lg/8">Shipping Policy</li>
+                <li className="text-lg/8">Sitemap</li>
+              </ul>
+            </div>
+            <div className="flex flex-col space-y-4">
+              <h2 className="text-3xl/8 font-bold">Location</h2>
+              <ul className="space-y-1">
+                <li className="text-lg/8">support@euphoria.in</li>
+                <li className="text-lg/8">
+                  Eklingpura Chouraha, Ahmedabad Main Road
+                </li>
+                <li className="text-lg/8">
+                  (NH 8- Near Mahadev Hotel) Udaipur, India- 313002
+                </li>
+              </ul>
+            </div>
+          </section>
+        </div>
+        <div>
+          <section></section>
+          <section></section>
+        </div>
+        <div className="">
+          <div className="border-euphoria-gray-400 border-y-[1px] px-20 py-8">
+            <span className="text-3xl/8 font-bold">Popular Categories</span>
+          </div>
+        </div>
+        <div>
+          <p className="text-center text-lg/8 font-bold">
+            Copyright &copy; 2023 Euphoria Folks Pvt Ltd. All rights reserved.
+          </p>
+        </div>
+      </div>
+    </>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,18 @@ import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: ['./src/**/*.{jsx,tsx}'],
-  theme: {},
+  theme: {
+    extend: {
+      colors: {
+        'euphoria-gray-800': '#3C4242',
+        'euphoria-gray-700': '#807D7E',
+        'euphoria-gray-400': '#BEBCBD',
+        'euphoria-gray-200': '#EDEEF2',
+        'euphoria-gray-100': '#F6F6F6',
+        'euphoria-purple-800': '#8A33FD',
+      },
+    },
+  },
   plugins: [],
 }
 export default config
